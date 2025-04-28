635437275b8a311f0aa5a4620f8b4bfdadca8a53
Ludvig Karlsson <ludka502@student.liu.se>
2025-04-28 14:49:10 +0200

    Uppdated files for lab 2

--- skel/chap2/llmultiply.asm
+++ skel/chap2/llmultiply.asm
@@ -35,11 +35,14 @@
 	;; Replace the zero below with the correct value to be able
 	;; to write your assembly code a little bit more readable: 
 	;;   MOV EAX, [EBP + BH_OFF]
-AL_OFF	EQU     0	; Offset from EBP to low  bits of a (AL)
-AH_OFF	EQU     0	; Offset from EBP to high bits of a (AH)
-BL_OFF	EQU     0	; Offset from EBP to low  bits of b (BL)
-BH_OFF	EQU     0	; Offset from EBP to high bits of b (BH)
-RES_OFF	EQU     0	; Offset from EBP to result array pointer
+AL_OFF	EQU     8	; Offset from EBP to low  bits of a (AL)
+AH_OFF	EQU     12	; Offset from EBP to high bits of a (AH)
+BL_OFF	EQU     16	; Offset from EBP to low  bits of b (BL)
+BH_OFF	EQU     20	; Offset from EBP to high bits of b (BH)
+RES_OFF	EQU     24	; Offset from EBP to result array pointer
+RES_OFF_2 EQU	28
+RES_OFF_3 EQU	32
+RES_OFF_4 EQU	36
 	;;    ^^^^^ Replace 0 with correct values above
         
 	GLOBAL llmultiply
@@ -47,9 +50,61 @@ RES_OFF	EQU     0	; Offset from EBP to result array pointer
 llmultiply:
 	PUSH EBP
 	MOV EBP, ESP
+	PUSH EAX
+	PUSH ECX
+	PUSH EDX
 
-	;; Put your implementation here
+
+	;;CLEAR
+	XOR EAX, EAX
+	XOR EDX, EDX
+	XOR ECX, ECX
+
+	;;POINTER
+	MOV ECX, [EBP + RES_OFF]
 
 
+	; a-LOW, b-LOW
+	MOV EAX, [EBP + AL_OFF]
+	MOV EDX, [EBP + BL_OFF]
+	MUL EDX
+
+	MOV [ECX], EAX
+	MOV [ECX + 4], EDX
+	
+
+	; a-LOW, b-HIGH
+	MOV EAX, [EBP + AL_OFF]
+	MOV EDX, [EBP + BH_OFF]
+	MUL EDX
+ 
+	MOV [ECX + 8], EDX
+	ADD [ECX + 4], EAX
+	ADC [ECX + 8], dword 0
+
+	
+	
+	; a-HIGH, b-LOW
+	MOV EAX, [EBP + AH_OFF]
+	MOV EDX, [EBP + BL_OFF]
+	MUL EDX
+
+	MOV [ECX + 12], dword 0
+	ADD [ECX + 4], EAX
+	ADC [ECX + 8], EDX
+	ADC [ECX + 12], dword 0
+
+	; a-HIGH, b-HIGH
+	MOV EAX, [EBP + AH_OFF]
+	MOV EDX, [EBP + BH_OFF]
+	MUL EDX
+
+	ADD [ECX + 8], EAX
+	ADC [ECX + 12], EDX
+	;TEST
+	
+	POP EDX
+	POP ECX
+	POP EAX
 	POP EBP				; restore EBP reg
 	RET				;  return
