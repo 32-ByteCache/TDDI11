bc600339ae8e68c676cc5e7236de37c03c41cf0d
32-ByteCache <cesspoolofdegeneracy@gmail.com>
2025-05-07 17:46:20 +0200

    Completed Lab 4

---
+++ Lab 4/packet.c
@@ -0,0 +1,78 @@
+#include <os_cpu.h>
+#include <os_cfg.h>
+#include <ucos_ii.h> /* os_c??.h must be included before ! */
+#include <libepc.h>
+
+#include "inbound.h"
+#include "elapsed.h"
+#include "serial.h"
+
+OS_EVENT *sem = NULL;
+
+void ReceivePackets(void)
+{
+  SerialInit() ;
+  
+  for (;;)
+  {
+    int type, byte, bytes ;
+    BYTE8 *bfr ;
+    
+    if (SerialGet() != 0xFF)
+      continue ;
+    
+    switch (type = SerialGet())
+    {
+      default:
+        continue ;
+      case 1:
+      case 2:
+        break ;
+    }
+    
+    bytes = SerialGet();
+    bfr = (BYTE8 *) malloc(bytes) ;
+    if (!bfr)
+    {
+      for (;;)
+        ;
+    }
+    
+    for (byte = 0; byte < bytes; byte++)
+    {
+      bfr[byte] = SerialGet() ;
+    }
+    switch (type)
+    {
+      case 1:
+        PostText(bfr) ;
+        break ;
+      case 2:
+        PostTime(bfr) ;
+        break ;
+    }
+  }
+}
+
+void SendPacket(int type, BYTE8 *bfr, int bytes)
+{
+  INT8U err;
+  if (sem == NULL) {
+    sem = OSSemCreate(1);
+  }
+  OSSemPend(sem, 0, &err);
+  
+  SerialPut(0xFF);
+  SerialPut(type);
+  
+  SerialPut(bytes);
+  OSTimeDly(10);
+  for (int i = 0; i < bytes; i++) {
+    SerialPut(bfr[i]);
+    OSTimeDly(10);
+  }
+  OSSemPost(sem);
+
+
+
+}
